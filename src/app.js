b2d6270624e79dfb9d72d1fd04a39f9bb14834a8
JYOTI BAMBLANI <75237287+JyotiBamblani@users.noreply.github.com>
2020-12-01 04:30:28 -0500

    adding script for projects

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require('path');
 const hbs = require('hbs');
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 // public static path
 const static_path = path.join(__dirname,"../public");
@@ -30,7 +30,7 @@ app.get("/weather", (req,res)=>{
 })
 app.get("*", (req,res)=>{
     res.render('404error',{
-        errorMsg:"Oops! Page Not Found,click to go back" 
+        errorMsg:"Oops! Page Not Found,click  here to go back" 
     });
 });
 
